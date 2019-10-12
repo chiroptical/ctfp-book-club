891ea7152757649246da7a27e603c8006947839e
Eric Schulte <eschulte@grammatech.com>
2019-10-12 13:25:03 -0400

    Chapter five exercises

--- exercises/eschulte/exercises.lisp
+++ exercises/eschulte/exercises.lisp
@@ -64,3 +64,23 @@
 ;;   0 bytes consed
 ;;
 ;; :DONE
+
+
+;;;; Chapter 5.
+(deftype either (left right)
+  `(or ,left ,right))
+
+(declaim (ftype (function (integer) integer) i)
+         (ftype (function (boolean) integer) j)
+         (ftype (function ((either integer boolean)) integer) m))
+(defun i (integer) integer)
+(defun j (boolean) (if boolean 1 0))
+(defun m (integer-or-boolean)
+  ;; m factorizes i and j because:
+  ;; i = [#'m #'identity-integer-to-either]
+  ;; j = [#'m #'identity-boolean-to-either]
+  ;; NOTE: in this case identity-integer-to-either and
+  ;; identity-boolean-to-either are both just identity
+  (etypecase integer-or-boolean
+    (integer integer-or-boolean)
+    (boolean (if integer-or-boolean 1 0))))
