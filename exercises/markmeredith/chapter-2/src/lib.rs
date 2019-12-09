c2c46884ecaf6a667cf5da20660b0907a08dd2fc
Mark Meredith <mark.meredith@netl.doe.gov>
2019-12-08 19:17:35 -0500

    try

--- exercises/markmeredith/chapter-2/src/lib.rs
+++ exercises/markmeredith/chapter-2/src/lib.rs
@@ -1,6 +1,59 @@
-pub fn memoize<A: 'static, B: 'static>(f: Box<dyn Fn(A) -> B>) -> Box<dyn Fn(A) -> B> {
-    //TODO....Didn't get around to it!!!
-    Box::new(move |x| f(x))
+use std::collections::HashMap;
+use std::hash::Hash;
+
+// pub fn memoize<A: 'static, B: 'static>(f: Box<dyn Fn(A) -> B>) -> Box<dyn Fn(A) -> B> {
+//     //TODO....Didn't get around to it!!!
+//     Box::new(move |x| f(x))
+// }
+
+struct MemoizedFunctionObject<A, R> {
+    data: HashMap<A, R>,
+    func: fn(&mut MemoizedFunctionObject<A, R>, A) -> R,
+}
+impl<A, R> MemoizedFunctionObject<A, R>
+where
+    A: Eq + Hash,
+{
+    fn from_func(func: fn(&mut Self, A) -> R) -> Self {
+        MemoizedFunctionObject {
+            data: HashMap::new(),
+            func: func,
+        }
+    }
+}
+
+impl<A, R> FnMut<(A,)> for MemoizedFunctionObject<A, R>
+where
+    A: Eq + Hash + Clone,
+    R: Clone,
+{
+    extern "rust-call" fn call_mut(&mut self, args: (A,)) -> Self::Output {
+        let arg = args.0;
+        match self.data.get(&arg).map(|x| x.clone()) {
+            Some(result) => result,
+            None => {
+                let result = (self.func.clone())(self, arg.clone());
+                self.data.insert(arg, result.clone());
+                result
+            }
+        }
+    }
+}
+
+fn memoize<A, R, F>(cache: &mut HashMap<A, R>, func: F, arg: A) -> R
+where
+    A: Eq + Hash + Clone,
+    R: Clone,
+    F: Fn(&mut HashMap<A, R>, A) -> R,
+{
+    match cache.get(&arg).map(|x| x.clone()) {
+        Some(result) => result,
+        None => {
+            let result = func(cache, arg.clone());
+            cache.insert(arg, result.clone());
+            result
+        }
+    }
 }
 
 #[cfg(test)]
